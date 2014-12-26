06ce9bad080b0638e4503c6208d692a107481fe1
Romain Francez <romain@bitsandco.com>
2014-12-26 12:37:28 +0100

    Refs #17 - Generates sections
    Waiting on refacto of submodules

---
+++ generators/section/index.js
@@ -0,0 +1,56 @@
+// Copyright (c) 2014 Bits & Co. and other generator-ng contributors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(function () {
+  'use strict';
+
+  var
+    NamedGenerator = require('../../util/NamedGenerator.js'),
+    path = require('path');
+    
+  module.exports = NamedGenerator.extend({
+    constructor: function () {
+      NamedGenerator.apply(this, arguments);
+      
+      this._formatName(['-section', '-sec']);
+      
+      this.composeWith('module', {
+        args: this.arguments, options: this.options
+      }, {
+        local: path.join(__dirname, '../module')
+      });
+      
+      // Get module
+      if (typeof this.options.dir === 'string') {
+        this.options.dir = path.join(this.options.dir, this.name);
+      }
+      if (typeof this.moduleName === 'string' && this.moduleName !== '') {
+          this.moduleName = this.module.name + '.';
+      } else {
+        this.moduleName = '';
+      }
+      this.moduleName += this.name;
+      
+      this.composeWith('controller', {
+        args: this.arguments, options: this.options
+      }, {
+        local: path.join(__dirname, '../controller')
+      });
+    },
+    
+    writing: function () {
+    }
+  });
+}());
